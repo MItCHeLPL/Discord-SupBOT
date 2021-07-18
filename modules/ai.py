914ad91dfbb418216d71d5ab75625eb44d84288e
Michał Myszor <45109417+MItCHeLPL@users.noreply.github.com>
2021-07-18 03:57:54 +0200

    added ai visibility changer

--- modules/ai.py
+++ modules/ai.py
@@ -9,7 +9,13 @@ load_dotenv()
 class Ai(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-        self.aiBot = self.bot.get_user(os.getenv('DISCORD_ID_AISUPBOT')) #get AI discord bot
+
+        for guild in bot.guilds:
+            if(guild.id == os.getenv('DISCORD_ID_BOBERSCHLESIEN')): #boberschlesien
+                self.aiBot = discord.utils.get(guild.members, id=(os.getenv('DISCORD_ID_AISUPBOT'))) #get AI discord bot
+                self.ai_category = discord.utils.get(guild.categories, id=(os.getenv('DISCORD_ID_AI_CATEGORY')))
+                self.bbsch_everyone_role = discord.utils.get(guild.roles, id=(os.getenv('DISCORD_ROLE_BBSCH_EVERYONE')) )
+
         self.lastMessageChannel = None
 
     #if got response from ai bot in dm's post it to last message channel
@@ -22,6 +28,14 @@ class Ai(commands.Cog):
     async def on_command_error(self, ctx, error):
         if isinstance(error, CommandNotFound):
             await self._send_dm_to_ai(ctx, ctx.message) #send ai response
+
+    @commands.Cog.listener()
+    async def on_voice_state_update(self, member, before, after):                
+        #show/hide AI category
+        if member == self.aiBot and member.status == discord.Status.online:
+            await self.ai_category.set_permissions(self.bbsch_everyone_role, view_channel = True, read_messages = True, send_messages = False)
+        if member == self.aiBot and member.status == discord.Status.offline:
+            await self.ai_category.set_permissions(self.bbsch_everyone_role, view_channel = False, read_messages = False, send_messages = False)
 
     #send dm to ai bot
     @commands.command(name = 'ai', aliases=['si'])
