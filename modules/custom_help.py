ddecc6aa0fc61e86879732636cb1cee298507dcb
Michał Myszor <45109417+MItCHeLPL@users.noreply.github.com>
2021-07-19 20:58:51 +0200

    added universal play command, improved dotenv call count and added preety custom help

--- modules/custom_help.py
+++ modules/custom_help.py
@@ -1,36 +1,19 @@
 import discord
 from discord.ext import commands
+from pretty_help import DefaultMenu, PrettyHelp
 
 class CustomHelp(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-        self.bot.remove_command('help') #remove default help command
+        #self.bot.remove_command('help') #remove default help command
 
-    # custom help command
-    @commands.command(name='help', aliases = ['pomoc', '?'])
-    async def _help(self, ctx, command = None):
-        if command == None:
-            helpEmbed = self.generateHelp(self.bot.commands) #if no command passed show all commands
-        else:
-            for botCommand in self.bot.commands:
-                if str(botCommand) == command or command in botCommand.aliases:
-                    if isinstance(botCommand, commands.Group):
-                        helpEmbed = self.generateHelp(botCommand.commands) #if passed command exists, show help about this command
-                        break
-                    else:
-                        pass
-                else:
-                    pass
+        #Preety help menu configuration
+        menu = DefaultMenu(page_left="⬅️", page_right="➡️", remove="✖️", active_time=45)
 
-        await ctx.reply(embed = helpEmbed) #reply with help message
+        #Preety help command configuration
+        self.bot.help_command = PrettyHelp(menu=menu, index_title="Kategorie", no_category="Bez kategorii", show_index=True)
 
-
-    def generateHelp(self, commandsArray):
-    helpText = '**Komendy:**\n\n'
-    for command in commandsArray:
-        helpText += f'''{command} {command.signature}{' ...' if isinstance(command, commands.Group) else ''}\n'''
-    return discord.Embed(title = 'Info', description = helpText)
 
 def setup(bot):
     bot.add_cog(CustomHelp(bot))
