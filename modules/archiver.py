b2f0610a0771463a01641676365d4019a99e0204
Michał Myszor <45109417+MItCHeLPL@users.noreply.github.com>
2022-04-06 21:51:05 +0200

    added support to multiple archive channels

--- modules/archiver.py
+++ modules/archiver.py
@@ -18,47 +18,20 @@ class Archiver(commands.Cog):
     @commands.Cog.listener()
     async def on_message(self, message):
         if message.author.bot is False and self.bot.settings["setting"]["archiver"]["archive_new_messeges"]:
-            archive_channel = discord.utils.get(self.bot.get_all_channels(), id=int(self.bot.settings["setting"]["archiver"]["archive_channel_id"])) #get archive channel
-
-            content = "\n◤━━━━━━━━━━━━━━\n"
-
-            #source info
-            content += ("**🟩New message** \n__🌐" + message.guild.name + "\n💬" + message.channel.name + "\n👤" + (str(message.author.name) + " #" + str(message.author.discriminator)) + "\n🆕" + str(datetime.datetime.utcnow())[0:-7] +  "__\n\n")
-
-            #text content
-            if message.content != "":
-                content += message.content + "\n"
-
-            #attachment content
-            for attachment in message.attachments:
-                content += attachment.url + "\n"
-
-            content += "\n"
-
-            await archive_channel.send(content=content)
-
-            if self.bot.settings["debug"]["archiver"]:
-               print(f'[{str(datetime.datetime.utcnow())[0:-7]}][archiver][on_message]Archived message\n')
-            
-
-    #removed message
-    @commands.Cog.listener()
-    async def on_raw_message_delete(self, payload):
-        if payload.cached_message is not None:
-            if payload.cached_message.author.bot is False and self.bot.settings["setting"]["archiver"]["archive_removed_messeges"]:
-                archive_channel = discord.utils.get(self.bot.get_all_channels(), id=int(self.bot.settings["setting"]["archiver"]["archive_channel_id"])) #get archive channel
+            for id in self.bot.settings["setting"]["archiver"]["archive_channel_ids"]:
+                archive_channel = discord.utils.get(self.bot.get_all_channels(), id=int(id)) #get archive channel
 
                 content = "\n◤━━━━━━━━━━━━━━\n"
 
                 #source info
-                content += ("**🟥New removed message** \n__🌐" + payload.cached_message.guild.name + "\n💬" + payload.cached_message.channel.name + "\n👤" + (str(payload.cached_message.author.name) + " #" + str(payload.cached_message.author.discriminator)) + "\n🆕" + payload.cached_message.created_at.strftime("%Y-%m-%d %H:%M:%S") + "\n🗑️" + str(datetime.datetime.utcnow())[0:-7] +  "__\n\n")
+                content += ("**🟩New message** \n__🌐" + message.guild.name + "\n💬" + message.channel.name + "\n👤" + (str(message.author.name) + " #" + str(message.author.discriminator)) + "\n🆕" + str(datetime.datetime.utcnow())[0:-7] +  "__\n\n")
 
                 #text content
-                if payload.cached_message.content != "":
-                    content += payload.cached_message.content + "\n"
+                if message.content != "":
+                    content += message.content + "\n"
 
                 #attachment content
-                for attachment in payload.cached_message.attachments:
+                for attachment in message.attachments:
                     content += attachment.url + "\n"
 
                 content += "\n"
@@ -66,7 +39,36 @@ class Archiver(commands.Cog):
                 await archive_channel.send(content=content)
 
                 if self.bot.settings["debug"]["archiver"]:
-                    print(f'[{str(datetime.datetime.utcnow())[0:-7]}][archiver][on_raw_message_delete]Archived removed message\n')
+                    print(f'[{str(datetime.datetime.utcnow())[0:-7]}][archiver][on_message]Archived message\n')
+            
+
+    #removed message
+    @commands.Cog.listener()
+    async def on_raw_message_delete(self, payload):
+        if payload.cached_message is not None:
+            if payload.cached_message.author.bot is False and self.bot.settings["setting"]["archiver"]["archive_removed_messeges"]:
+                for id in self.bot.settings["setting"]["archiver"]["archive_channel_ids"]:
+                    archive_channel = discord.utils.get(self.bot.get_all_channels(), id=int(id)) #get archive channel
+
+                    content = "\n◤━━━━━━━━━━━━━━\n"
+
+                    #source info
+                    content += ("**🟥New removed message** \n__🌐" + payload.cached_message.guild.name + "\n💬" + payload.cached_message.channel.name + "\n👤" + (str(payload.cached_message.author.name) + " #" + str(payload.cached_message.author.discriminator)) + "\n🆕" + payload.cached_message.created_at.strftime("%Y-%m-%d %H:%M:%S") + "\n🗑️" + str(datetime.datetime.utcnow())[0:-7] +  "__\n\n")
+
+                    #text content
+                    if payload.cached_message.content != "":
+                        content += payload.cached_message.content + "\n"
+
+                    #attachment content
+                    for attachment in payload.cached_message.attachments:
+                        content += attachment.url + "\n"
+
+                    content += "\n"
+
+                    await archive_channel.send(content=content)
+
+                    if self.bot.settings["debug"]["archiver"]:
+                        print(f'[{str(datetime.datetime.utcnow())[0:-7]}][archiver][on_raw_message_delete]Archived removed message\n')
 
 def setup(bot):
     bot.add_cog(Archiver(bot))
